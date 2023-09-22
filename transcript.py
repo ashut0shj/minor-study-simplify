dc7aa02a7d08a9e69621f3b7a3be386ce934e3bd
ashut0sh <ashutoshj2810@gmail.com>
2023-09-22 20:13:40 +0530

    added class

---
+++ transcript.py
@@ -0,0 +1,27 @@
+import moviepy.editor as mp
+import assemblyai as aai
+import os
+
+with open("api_key.txt") as file:
+    file_content = file.readline()
+    api_key = file_content
+
+class VideoTranscriber:
+
+    aai.settings.api_key = api_key
+
+    def __init__(self,video_file_path):
+        self.transcriber = aai.Transcriber()
+        self.video_file_path = video_file_path
+
+    def convert_to_video(self):
+        video_clip = mp.VideoFileClip(self.video_file_path)
+        video_clip.audio.write_audiofile('temp.wav')
+
+    def transcribe(self):
+        transcript = self.transcriber.transcribe(r"temp.wav")
+        os.remove('temp.wav')
+        
+        return transcript.text
+
+v = VideoTranscriber("a")
