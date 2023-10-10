8e2fb077aff15d5149d8f8e724646303d79cec94
Swasti <srivastavaswasti7@gmail.com>
2023-10-10 23:09:58 +0530

    lulu

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request
-from transcript import VideoTranscriber
+from transcript import Transcriber
 
 '''todo add mime'''
 
@@ -7,16 +7,21 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    def get_content_type():
+        content_type = request.content_type
+        return f"Content Type: {content_type}"
+
     if request.method == 'POST':
         video = request.files['video']
         media_type = video.content_type.split("/")
         file = 'temp.' + media_type[1]
         video.save(file)
-        media = VideoTranscriber(file)
+        media = Transcriber(file)
 
         if media_type[0] == 'video':
             media.convert_to_audio()
             
+        print(get_content_type)
         input("Press enter to continue with transcription")
         
         transcript = media.transcribe()
