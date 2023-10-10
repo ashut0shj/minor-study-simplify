28a9fa11aa6906e1855593b24f79cb761713a5e4
kanika1-13 <kanika22102@iiitnr.edu.in>
2023-10-11 04:09:38 +0530

    kannu

--- summariser.py
+++ summariser.py
@@ -1,16 +1,6 @@
-import transformers
-from transformers import T5ForConditionalGeneration, T5Tokenizer
-
-model = T5ForConditionalGeneration.from_pretrained('t5-small')
-tokenizer = T5Tokenizer.from_pretrained('t5-small')
-
-text = input("Enter the text you want to summarize: ")
-max_length = 20
-
-input_text = "summarize: " + text
-input_ids = tokenizer.encode(input_text, return_tensors='pt')
-
-summary = model.generate(input_ids, max_length=max_length)
-
-summary_text = tokenizer.decode(summary[0], skip_special_tokens=True)
-print("Summary:", summary_text)
+from transformers import PegasusForConditionalGeneration , PegasusTokenizer
+model_name = "google/pegasus-xsum"
+pegasus_tokenizer = PegasusTokenizer.from_pretrained(model_name)
+pegasus_model = PegasusForConditionalGeneration.from_pretrained(model_name)
+tokens = pegasus_tokenizer(example_text, truncation = True , padding = "longest" , return_tensors = "pt")
+tokens
