026d27a6d4f0c32e5d4f361efe58d73ac6bb15e1
ashut0sh <ashutoshj2810@gmail.com>
2023-10-11 00:27:49 +0530

    chota baccha hu mai, mimetype already done tha

--- app.py
+++ app.py
@@ -7,21 +7,18 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    def get_content_type():
-        content_type = request.content_type
-        return f"Content Type: {content_type}"
 
     if request.method == 'POST':
         video = request.files['video']
         media_type = video.content_type.split("/")
         file = 'temp.' + media_type[1]
         video.save(file)
+
         media = Transcriber(file)
 
         if media_type[0] == 'video':
             media.convert_to_audio()
-            
-        print(get_content_type)
+
         input("Press enter to continue with transcription")
         
         transcript = media.transcribe()
