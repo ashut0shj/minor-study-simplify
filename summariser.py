9252598f7c75203c64d7947f3f9fdf12e9786b95
kanika1-13 <kanika22102@iiitnr.edu.in>
2023-10-11 04:12:55 +0530

    kannu

--- summariser.py
+++
@@ -1,6 +0,0 @@
-from transformers import PegasusForConditionalGeneration , PegasusTokenizer
-model_name = "google/pegasus-xsum"
-pegasus_tokenizer = PegasusTokenizer.from_pretrained(model_name)
-pegasus_model = PegasusForConditionalGeneration.from_pretrained(model_name)
-tokens = pegasus_tokenizer(example_text, truncation = True , padding = "longest" , return_tensors = "pt")
-tokens
