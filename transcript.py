d85cc4e8d763eedbfc2b0101f7221f6df7974ec0
ashut0sh <ashutoshj2810@gmail.com>
2023-10-11 00:43:45 +0530

    mime done

--- transcript.py
+++ transcript.py
@@ -15,14 +15,16 @@ class Transcriber:
     def __init__(self,video_file_path):
         self.transcriber = aai.Transcriber()
         self.video_file_path = video_file_path
-
-    def convert_to_audio(self):
+        self.media_type = video_file_path.split(".")[-1]
+        print(self.media_type)
+        
+    def video_transcribe(self):
         video_clip = mp.VideoFileClip(self.video_file_path)
         video_clip.audio.write_audiofile('temp.wav')
         video_clip.close()
         os.remove('temp.mp4')
 
-    def transcribe(self):
+    def audio_transcribe(self):
         transcript = self.transcriber.transcribe(r"temp.wav")
         os.remove('temp.wav')
 
